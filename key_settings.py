8fcac16ae370bc720e2444ee24ac381f19cd461d
Pavel Zaiats <Pavel_Zaiats8503@jabil.com>
2022-12-04 16:16:32 -0800

    typo mistake fix

--- key_settings.py
+++ key_settings.py
@@ -1,3 +1,3 @@
-# file should be added to .gitignore (but for demo propose present in repo)
+# file should be added to .gitignore (but for demo purpose present in repo)
 
 PLANET_API_KEY = "PLAK0f3d53334df748b9809544a315501bf4"
